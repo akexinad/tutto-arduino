d8a928a6d1ce0a44b90ee01503a1c7fd413c0a82
akexinad <danixeka@gmail.com>
2019-04-02 17:44:21 +1100

    added potentiometer to control volume

--- projects/06-light-theramin/06-light-theramin.ino
+++ projects/06-light-theramin/06-light-theramin.ino
@@ -3,6 +3,10 @@ int sensorLow = 1023;
 int sensorHigh = 0;
 const int ledPin = 13;
 
+int const potPin = A1;
+int potValue;
+int volume;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
@@ -18,12 +22,16 @@ void setup() {
   }
   digitalWrite(ledPin, LOW);
 
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   sensorValue = analogRead(A0);
+  potValue = analogRead(potPin);
+  Serial.println("potValue: ");
+  Serial.println(potValue);
   int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
   tone(8, pitch, 20);
-  delay(10);
+  delay(1000);
 }
